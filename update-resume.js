c49aa3d2e80924a754eb2f3a29a8d428583e7a9d
Артем <artemdyakov19@gmail.com>
2020-10-14 20:06:32 +0300

    first commit

---
+++ update-resume.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs').promises;
+
+const UserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36';
+const ResumesID = [
+  '<id resume>',
+  '<id resume>'
+]
+
+
+async function getCookie() {
+  const browser = await puppeteer.launch( {headless: false} )
+  const page = await browser.newPage()
+  await page.setUserAgent(UserAgent)
+  await page.goto('https://spb.hh.ru/login')
+  const cookies = await page.cookies()
+  await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2))
+  await browser.close()
+}
+
+async function updateResume(resumeID) {
+  const browser = await puppeteer.launch( {headless: false, slowMo: 20} )
+  const page = await browser.newPage()
+  await page.setUserAgent(UserAgent)
+  const cookiesString = await fs.readFile('./cookies.json')
+  const cookies = JSON.parse(cookiesString)
+  await page.setCookie(...cookies)
+  await page.goto('https://spb.hh.ru/resume/'+resumeID)
+  await page.click('[data-qa="resume-update-button"]')
+  await browser.close()
+}
+
+if (process.argv[2] == '--get-cookies') { 
+  getCookie()
+} 
+else {
+  ResumesID.map(updateResume)
+}
+
